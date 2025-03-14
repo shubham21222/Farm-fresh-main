76571f98a928ae2dbb028e6079d86ff93e616f1a
shubham21222 <shubhamraikwar@hotmail.com>
2025-03-14 10:02:22 +0530

    Update Next.js configuration for standalone output and enable trailing slashes; modify build script to include export step.

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    output: 'standalone',
+    trailingSlash: true,
     images: {
         domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com','via.placeholder.com','bid.nyelizabeth.com','hebbkx1anhila5yf.public.blob.vercel-storage.com','i.ibb.co' ,'img1.wsimg.com' ,'auction.nyelizabeth.com', 'beta.nyelizabeth.com', 'images.liveauctioneers.com', 'p1.liveauctioneers.com','source.unsplash.com','images.unsplash.com','images.pexels.com','cdn.pixabay.com'], // Add other domains if needed
     }
-
 };
 
 export default nextConfig;
